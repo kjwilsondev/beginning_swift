9801a66acc45eee9fb318be6563c9d8a50ddd2d7
KJ Wilson <yourboykjdev@gmail.com>
2020-01-28 15:16:41 -0800

    variables float

---
+++ variables_float.swift
@@ -0,0 +1,7 @@
+var pi: Double = 3.14
+var diameter: Double = 3
+var radius = diameter / 2
+// var circleArea = pow(radius, Double(2)) * pi
+var circleArea = radius * radius * pi
+
+print(circleArea)
